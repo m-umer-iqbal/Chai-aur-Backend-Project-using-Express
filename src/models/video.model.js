c542f5088dacfd282ef90c102a275c184a8d9d61
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-03-13 22:23:36 +0500

    Install mongoose aggregate paginate v2 bcrypt and jwt package and add mongoose plugin in videos model and use pre hook to hash password before save and write custom methods to generate access and refresh token

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema({
     videoFile: {
@@ -38,5 +39,7 @@ const videoSchema = new mongoose.Schema({
         ref: "User",
     }
 }, { timestamps: true });
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
