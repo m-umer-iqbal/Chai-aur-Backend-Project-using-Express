089e688dd180724a8e2d2473a446e6687417c5c1
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-07-16 06:42:45 +0500

    Add the filefilter and limit on multer file uploading

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -7,8 +7,30 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
-})
+});
 
-const uploadOnServerByMulter = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = [
+        "image/jpeg",
+        "image/png",
+        "image/webp"
+    ];
+
+    if (!allowedTypes.includes(file.mimetype)) {
+        return cb(new Error("Only JPG, PNG and WEBP images are allowed."));
+    }
+
+    cb(null, true);
+};
+
+const uploadOnServerByMulter = multer(
+    {
+        storage: storage,
+        limits: {
+            fileSize: 5 * 1024 * 1024 // 5 MB
+        },
+        fileFilter
+    }
+);
 
 export default uploadOnServerByMulter;
