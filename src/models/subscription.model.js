ef5e5ba6b2495e522e414d1a3242ce5f85fafa6d
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-07-17 05:12:19 +0500

    Remove the array from the subscriber field in the subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
-    subscriber: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }
-    ],
+    subscriber: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
     channel: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
